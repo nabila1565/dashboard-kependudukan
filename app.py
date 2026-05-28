e9844b7b68ae3813155f535cd9af24000e93010f
Nabila Agatha Parsa <nabilanapp15@gmail.com>
2026-05-28 12:20:03 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 import streamlit as st
-from informasi_jawa_timur import show_informasi_jawa_timur
 
 st.set_page_config(
     page_title="Dashboard Demografi Jawa Timur",
@@ -9,31 +8,26 @@ st.set_page_config(
 
 st.markdown("""
 <style>
-/* Background utama aplikasi */
 .stApp {
     background-color: #FFFFFF;
 }
 
-/* Header atas Streamlit */
 [data-testid="stHeader"] {
     background-color: #FFFFFF;
 }
 
-/* Sidebar */
 [data-testid="stSidebar"] {
-    background: #306D29
+    background: #111844;
 }
 
-/* Judul/label sidebar */
 [data-testid="stSidebar"] label {
     color: white !important;
     font-weight: 600;
 }
 
-/* Radio menu KHUSUS di sidebar */
 [data-testid="stSidebar"] div[role="radiogroup"] label {
-    background-color: none;
-    border: 1.5px solid white !important;
+    background-color: #111844 !important;
+    border: 1.5px solid #5DADE2 !important;
     padding: 14px 16px;
     border-radius: 8px;
     margin-bottom: 12px;
@@ -41,34 +35,18 @@ st.markdown("""
     box-shadow: 0 4px 12px rgba(0,0,0,0.25);
 }
 
-/* Hover radio menu KHUSUS sidebar */
 [data-testid="stSidebar"] div[role="radiogroup"] label:hover {
     background-color: rgba(77,166,255,0.35) !important;
     transform: translateX(4px);
 }
 
-/* Teks radio menu KHUSUS sidebar */
 [data-testid="stSidebar"] div[role="radiogroup"] label p {
     color: white !important;
     font-size: 16px;
     font-weight: 600;
 }
-
-/* Pastikan area utama tidak ikut putih */
-section.main p,
-section.main h1,
-section.main h2,
-section.main h3,
-section.main h4,
-section.main h5,
-section.main h6,
-section.main label,
-section.main span {
-    color: inherit;
-}
 </style>
 """, unsafe_allow_html=True)
-
 
 menu = st.sidebar.radio(
     "Pilih Menu",
@@ -81,14 +59,21 @@ menu = st.sidebar.radio(
 )
 
 if menu == "Informasi Jawa Timur":
+    from informasi_jawa_timur import show_informasi_jawa_timur
     show_informasi_jawa_timur()
 
 elif menu == "Dashboard":
     from dashboard import show_page
     show_page()
+
 elif menu == "Dataset":
     from Dataset import show_page
     show_page()
+
 elif menu == "Hasil Cluster Tahun 2025":
-    from hasil_cluster import show_page
-    show_page()
+    try:
+        from hasil_cluster import show_page
+        show_page()
+    except Exception as e:
+        st.error("Halaman Hasil Cluster belum bisa ditampilkan.")
+        st.write("Detail error:", e)
